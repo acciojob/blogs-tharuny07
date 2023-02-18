6a7513600f7fbfa9127e311dac5c6ce123bba133
Tharun Gorla <tharunsoula@gmail.com>
2023-02-18 14:40:50 +0530

    First solution

---
+++ src/main/java/com/driver/models/Blog.java
@@ -0,0 +1,77 @@
+package com.driver.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "blog")
+public class Blog{
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String title;
+        private String content;
+        private Date pubDate;
+
+        @ManyToOne
+        @JoinColumn
+        private User user;
+
+        @OneToMany(mappedBy = "blog",cascade = CascadeType.ALL)
+        private List<Image> imageList=new ArrayList<>();
+
+        public Blog() {
+        }
+
+        public List<Image> getImageList() {
+                return imageList;
+        }
+
+        public void setImageList(List<Image> imageList) {
+                this.imageList = imageList;
+        }
+
+        public User getUser() {
+                return user;
+        }
+
+        public void setUser(User user) {
+                this.user = user;
+        }
+
+        public int getId() {
+                return id;
+        }
+
+        public void setId(int id) {
+                this.id = id;
+        }
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public String getContent() {
+                return content;
+        }
+
+        public void setContent(String content) {
+                this.content = content;
+        }
+
+        public Date getPubDate() {
+                return pubDate;
+        }
+
+        public void setPubDate(Date pubDate) {
+                this.pubDate = pubDate;
+        }
+        //Kan@2911
+}
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,40 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+         Blog blog=blogRepository2.findById(blogId).get();
 
+         Image image=new Image();
+         image.setBlog(blog);
+         image.setDescription(description);
+         image.setDimensions(dimensions);
+
+         List<Image> imageList=blog.getImageList();
+         imageList.add(image);
+         blogRepository2.save(blog);
+
+         return image;
     }
 
     public void deleteImage(Integer id){
-
+             imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        int count=0;
+         Image image=imageRepository2.findById(id).get();
+         String imageDimensions=image.getDimensions();
+         int midIndexOfScreen=screenDimensions.indexOf('X');
+         int midIndexOfImage=imageDimensions.indexOf('X');
 
+         int imageArea=Integer.parseInt(imageDimensions.substring(0,midIndexOfImage))*Integer.parseInt(imageDimensions.substring(midIndexOfImage+1));
+         int screenArea=Integer.parseInt(screenDimensions.substring(0,midIndexOfScreen))*Integer.parseInt(screenDimensions.substring(midIndexOfScreen+1));
+         int n=imageArea;
+         while(n<=screenArea)
+         {
+             count++;
+             n+=imageArea;
+         }
+         return count;
     }
 }
